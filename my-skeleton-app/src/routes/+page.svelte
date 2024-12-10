0cf27f7b5eafe8247088bf70c1238ba3e4f8b508
porant <porant@gmail.com>
2024-12-10 23:49:14 +0100

    first commit

---
+++ my-skeleton-app/src/routes/+page.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+	import Header from "./Header.svelte";
+	import Bottom from "./Bottom.svelte";
+	import Logo from "./Logo.svelte";
+	import MyAnims from "./MyAnims.svelte";
+	import {
+	blur,
+	crossfade,
+	draw,
+	fade,
+	fly,
+	scale,
+	slide
+} from 'svelte/transition';
+
+	
+	let menu_value = 1;
+
+
+</script>
+<head>
+    <link href="./../node_modules/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet"> 
+</head>
+<Header bind:menu_value/>
+<body>
+	
+
+		{#if menu_value == 0}
+		<MyAnims />
+		{/if}
+
+		{#if menu_value != 0}
+			<span out:fly={{delay: 500, x:-100, duration:200}}><Logo /></span>
+		{/if}
+
+		{#if menu_value == 2}
+			<div class="construction"><span class=""><i class="fa-solid fa-skull" /> UNDER CONSTRUCTION <i class="fa-solid fa-skull" /></span>
+				<section class="card w-full">
+					<div class="p-4 space-y-4">
+						<div class="placeholder" />
+						<div class="grid grid-cols-3 gap-8">
+							<div class="placeholder" />
+							<div class="placeholder" />
+							<div class="placeholder" />
+						</div>
+						<div class="grid grid-cols-4 gap-4">
+							<div class="placeholder" />
+							<div class="placeholder" />
+							<div class="placeholder" />
+							<div class="placeholder" />
+						</div>
+					</div>
+				</section>
+				<span class=""><i class="fa-solid fa-skull" /> UNDER CONSTRUCTION <i class="fa-solid fa-skull" /></span>
+			</div>
+		{/if}
+
+	
+</body>
+<Bottom />
+
+<style>
+
+
+	.construction{
+		color: rgb(238, 255, 0);
+		background-color: rgb(71, 12, 12);
+		
+		display: grid;
+		position: absolute;
+		top: 200px;
+		width: 100%;
+		text-align: center;
+	}
+</style>
---
+++ my-skeleton-app/src/routes/Header.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { RadioGroup, RadioItem } from "@skeletonlabs/skeleton";
+
+    export let menu_value:number;
+
+
+</script>
+
+<header>
+    <RadioGroup active="variant-filled-primary" hover="hover:variant-soft-primary">
+        <RadioItem bind:group={menu_value} name="justify" value={0}>ANIM</RadioItem>
+	    <RadioItem bind:group={menu_value} name="justify" value={1}>HOME</RadioItem>
+	    <RadioItem bind:group={menu_value} name="justify" value={2}>DEV</RadioItem>    
+    </RadioGroup>
+</header>
+
+
+<style>
+    header {
+        z-index: 7;
+        position: fixed;
+        display: flex;
+        justify-content: center;
+        top: 30px;
+        width: 100%;
+        padding: 10px;
+    }
+    
+
+</style>
